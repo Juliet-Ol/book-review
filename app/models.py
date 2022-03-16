19aa3bd85b6da99cc7e5c7a2c3b52139f8a3407a
Paul Munyao <paulmunyao094@gmail.com>
2022-03-16 04:30:43 +0300

    Create a class for our reviews

--- app/models.py
+++ app/models.py
@@ -7,7 +7,11 @@ class Book:
         self.publisher = publisher
         self.description = description
 
-# class Review:
-#     all_reviews = []
-#     def __init__(self,book_id,title,image_url,review):
+class Review:
+    all_reviews = []
+    def __init__(self,book_id,title,image_url,review):
+        self.book_id = book_id
+        self.title = title
+        self.image_url = image_url
+        self.review = review
 
