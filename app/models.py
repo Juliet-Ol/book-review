2c806738d0e7b4bd5423a0e83cf76d88b79a3b8c
Paul Munyao <paulmunyao094@gmail.com>
2022-03-16 04:28:37 +0300

    Create a class for our book object

---
+++ app/models.py
@@ -0,0 +1,13 @@
+class Book:
+
+    def __init__(self, id,title,author,publisher,description):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.description = description
+
+# class Review:
+#     all_reviews = []
+#     def __init__(self,book_id,title,image_url,review):
+
