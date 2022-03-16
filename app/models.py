9928452065d24132c9ec3b7a28b54afdf4020021
Paul Munyao <paulmunyao094@gmail.com>
2022-03-16 11:39:23 +0300

    Modify files

--- app/models.py
+++ app/models.py
@@ -1,33 +1,33 @@
-# class Book:
+class Book:
 
-#     def __init__(self, id,title,author,publisher,description):
-#         self.id = id
-#         self.title = title
-#         self.author = author
-#         self.publisher = publisher
-#         self.description = description
+    def __init__(self, id,title,author,publisher,description):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.description = description
 
-# class Review:
-#     all_reviews = []
-#     def __init__(self,book_id,title,image_url,review):
-#         self.book_id = book_id
-#         self.title = title
-#         self.image_url = image_url
-#         self.review = review
+class Review:
+    all_reviews = []
+    def __init__(self,book_id,title,image_url,review):
+        self.book_id = book_id
+        self.title = title
+        self.image_url = image_url
+        self.review = review
 
-#     def save_review(self):
-#         Review.all_reviews.append(self)  
+    def save_review(self):
+        Review.all_reviews.append(self)  
 
-#     @classmethod  
-#     def clear_reviews(cls):
-#         Review.all_reviews.clear()
+    @classmethod  
+    def clear_reviews(cls):
+        Review.all_reviews.clear()
 
-#     @classmethod
-#     def get_reviews(cls,id):  
-#         response =[]  
-#         for review in cls.all_reviews:
-#             if review.id == id:
-#                 response.append(review)
+    @classmethod
+    def get_reviews(cls,id):  
+        response =[]  
+        for review in cls.all_reviews:
+            if review.id == id:
+                response.append(review)
 
-#         return response        
+        return response        
 
