9b43ef23c80ba9054d36d376b9cfbdf1a8342050
Paul Munyao <paulmunyao094@gmail.com>
2022-03-17 06:07:18 +0300

    Add  and style button links

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,5 +4,6 @@ from flask import render_template
 @main.route('/')
 @main.route('/index')
 def index():
-    users = {'username': 'Hey there Get to see and review your top favorite books at the comfort of your couch'}
-    return render_template('index.html',users =users)
+    user = {'username': 'Hey there Get to see and review your top favorite books at the comfort of your couch'}
+    users = {'usersname':'Already have an account ?'}
+    return render_template('index.html',user =user,users=users)
