3a5bc66067c9b1b5d56be69f2bb5a92b84bd6bcb
Paul Munyao <paulmunyao094@gmail.com>
2022-03-17 06:20:17 +0300

    Add registration forms

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,3 +7,7 @@ def index():
     user = {'username': 'Hey there Get to see and review your top favorite books at the comfort of your couch'}
     users = {'usersname':'Already have an account ?'}
     return render_template('index.html',user =user,users=users)
+
+@main.route('/register')
+def register():
+    return render_template('register.html')    
