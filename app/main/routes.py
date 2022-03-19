b0e7579f43e49633ab42c0b86caa24883929c2d1
Paul Munyao <paulmunyao094@gmail.com>
2022-03-19 11:40:19 +0300

    Commiting new changes to database

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
 from . import main
 from flask import render_template,flash,redirect,url_for
 from flask_login import logout_user
+from app import db
 from app.models import User
 from  .forms import LoginForm,RegistrationForm
 from .. requests import get_random_books
@@ -9,7 +10,6 @@ from .. requests import get_random_books
 @main.route('/')
 @main.route('/index')
 def index():
-    book = get_random_books()
     word = {'username': 'Hey there Get to see and review your top favorite books at the comfort of your couch'}
     words = {'usersname':'Already have an account ?'}
     return render_template('index.html',word =word,words=words)
@@ -17,6 +17,11 @@ def index():
 @main.route('/register',methods=['POST','GET'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user_create = User(username=form.username.data,email=form.email.data,password=form.password.data)
+        db.session.add(user_create)
+        db.session.commit()
+        flash(f"Account created successfully for{form.name.data}")
     return render_template('register.html',title='Create your account',form=form)
 
 @main.route('/login',methods=['POST','GET'])
@@ -29,6 +34,7 @@ def login():
 
 @main.route('/home')    
 def home ():
+    book = get_random_books()
     wor = {'names':'Grab yourself some nice cup of coffee as we nit pick'}
     return render_template('home.html',wor=wor)  
 
