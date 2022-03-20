7ffeb9cbe8696bc32d70baa56099bbe49ad84824
Paul Munyao <paulmunyao094@gmail.com>
2022-03-20 11:54:01 +0300

    Modified files

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,20 @@
 from flask_wtf import FlaskForm
+from app.models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired,ValidationError,Email,EqualTo
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("That email is taken. Please choose a different one")
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,47 +1,60 @@
 from . import main
-from flask import render_template,flash,redirect,url_for
-from flask_login import logout_user
+import urllib.request
+import json
+from flask import render_template, flash, redirect, url_for
+from flask_login import logout_user,login_user,login_required
 from app import db
 from app.models import User
-from  .forms import LoginForm,RegistrationForm
-from .. requests import get_random_books
+from .forms import LoginForm, RegistrationForm
+
 
 
 @main.route('/')
 @main.route('/index')
 def index():
     word = {'username': 'Hey there Get to see and review your top favorite books at the comfort of your couch'}
-    words = {'usersname':'Already have an account ?'}
-    return render_template('index.html',word =word,words=words)
+    words = {'usersname': 'Already have an account ?'}
+    return render_template('index.html', word=word, words=words)
 
-@main.route('/register',methods=['POST','GET'])
+
+@main.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user_create = User(username=form.username.data,email=form.email.data,password=form.password.data)
+        user_create = User(username=form.username.data,
+                           email=form.email.data, password=form.password.data)
         db.session.add(user_create)
         db.session.commit()
-        flash(f"Account created successfully for{form.name.data}")
-        return redirect('/home')
-    return render_template('register.html',title='Create your account',form=form)
+        flash(f"Account created successfully for{form.username.data}")
+        return redirect('/login')
+    return render_template('register.html', title='Create your account', form=form)
 
-@main.route('/login',methods=['POST','GET'])
+
+@main.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {},remember_me: {}'.format(form.username.data, form.remember_me.data))
-        return redirect('/home')
-    return render_template('login.html', title='Login',form=form)   
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            return redirect('/home')
+        flash('Login requested for user {},remember_me: {}'.format(
+            form.email.data, form.remember_me.data)) 
+    return render_template('login.html', title='Login', form=form)
 
-@main.route('/home')    
-def home ():
-    book = get_random_books()
-    wor = {'names':'Grab yourself some nice cup of coffee as we nit pick'}
-    return render_template('home.html',wor=wor)  
+
+@main.route('/home')
+def home():
+    url = 'https://www.googleapis.com/books/v1/volumes?q=isbn'
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    wor = {'names': 'Grab yourself some nice cup of coffee as we nit pick'}
+    return render_template('home.html', wor=wor,books=dict["items"])
+
 
 @main.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('main.index'))
-
-     
